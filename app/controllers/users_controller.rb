cd81e351eb96c5b71f9d3569401d2091d5cd4787
anikachristopher <anika.mchristoper@gmail.com>
2020-02-25 00:46:38 -0500

    build user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,35 @@ class UsersController < ApplicationController
             redirect '/' 
         else 
             @errors = ["Please enter the required information"]
+            erb :failure
+        end
+    end
+
+    get '/login' do
+        erb :login
+    end
+
+    post '/login' do
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] =  @user.id 
+            redirect '/profile'
+        else
+            @errors = ["Please enter valid information"]
+            erb :failure
+        end
+    end
+
+    get '/signout' do
+        redirect_if_not_logged_in
+        session.clear 
+        redirect '/'
+    end
+
+    get '/profile' do
+        redirect_if_not_logged_in
+        @user = User.find(session[:user_id])
+        erb :profile
     end
 
     private
