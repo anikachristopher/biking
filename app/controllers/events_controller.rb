89417e54f4485bb453613496aafd8a89d6138f03
anikachristopher <anika.mchristoper@gmail.com>
2020-02-25 01:21:24 -0500

    build events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,13 +12,65 @@ class EventsController < ApplicationController
 
     post '/events' do
         redirect_if_not_logged_in
-        @event = current_user.events.build
+        @event = current_user.events.build(event_params)
+        @event.user = User.find(session[:user_id])
+        if @event.save
+            erb :show
+        else 
+            event_params.nil? || event_params.empty?
+            @errors = ["Event is Invalid"]
+            erb :failure
+        end
+    end
+
+    get '/events/:id' do
+        @event = Event.find(params[:id])
+        erb :show
+    end
+
+    get '/events/:id/edit' do
+        redirect_if_not_logged_in
+        if current_user 
+            @event = current_user.events.find_by(id: params[:id])
+            if @event 
+                erb :edit
+            else 
+                redirect '/'
+            end
+        else 
+            redirect '/'
+        end
+    end
+
+    patch '/events/:id' do 
+        redirect_if_not_logged_in
+        @event = current_user.events.find_by(id: params[:id])
+        @event.date = params[:date] 
+        @event.location = params[:location]
+        @event.time = params[:time]
+        @event.save 
+        erb :show
+    end
+
+    delete '/events/:id' do
+        redirect_if_not_logged_in
+        if current_user
+            @event = current_user.events.find_by(id: params[:id])
+            if @event
+                @event.delete
+                redirect '/profile'
+            else 
+                redirect '/'
+            end
+             else 
+            redirect '/'
+        end
     end
 
         private 
 
-        def event_params
-            {date: params[:date], location: params[:location], time: params[:time]}
-        end
+    def event_params
+        {date: params[:date], location: params[:location], time: params[:time]}
+    end
 
     end
