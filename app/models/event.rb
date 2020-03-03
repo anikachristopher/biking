d8568488249267048f4c09ff2bf6fcf2d0483dcd
anikachristopher <anika.mchristoper@gmail.com>
2020-03-02 23:33:50 -0500

    created validation for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
     belongs_to :user
     
-        validates :location, :date, presence: true
+        validates :location, :date, :event_name, presence: true
+        validates :event_name, uniqueness: true
           
     end
